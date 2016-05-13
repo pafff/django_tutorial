38106b3b46df922c39064e999ca8ec0060dfd784
Paweł Ostrowski <oczomb@gmail.com>
2016-05-13 16:20:59 +0200

    Add tests Corrections to models and views

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.core.urlresolvers import reverse
+from django.utils import timezone
 from django.views import generic
 
 from models import Choice, Question
@@ -11,17 +12,34 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).exclude(
+            choice__isnull=True).order_by('-pub_date').distinct()[:5]
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
 
+    def get_queryset(self):
+        """
+            Excludes any questions that aren't published yet
+            or have no choices.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now()).exclude(
+            choice__isnull=True)
+
 
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
+
+    def get_queryset(self):
+        """
+            Excludes any questions that aren't published yet
+            or have no choices.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now()).exclude(
+            choice__isnull=True)
 
 
 def vote(request, question_id):
